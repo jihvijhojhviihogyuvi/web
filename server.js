bc23d1ba07d9032810f032bd8ac52169e29e4ac9
jihvijhojhviihogyuvi <wolfe.charlie.r@gmail.com>
2026-02-08 10:38:30 -0500

    Update server.js to use puppeteer-core

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 const path = require('path');
 const app = express();
